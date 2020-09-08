c222a2dd15e9fed3b88f48a2083a415e3355de35
unknown <samuelyu.kw@gmail.com>
2020-09-09 03:45:26 +1000

    shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import './App.css'
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
-
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   )
 }
