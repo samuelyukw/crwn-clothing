d8f1ec2a6355aedc9be6363afdb17379e68c4a5e
unknown <samuelyu.kw@gmail.com>
2020-09-09 17:53:27 +1000

    header, signin, form input and custom button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 import './App.css'
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
+    <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignOut} />
+      </Switch>
     </div>
   )
 }
