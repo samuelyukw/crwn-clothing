0b6f2b6761332868985ac4e77022629d6991acc3
unknown <samuelyu.kw@gmail.com>
2020-09-12 01:22:48 +1000

    reslector for cartItems

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {selectCartItems} from '../../redux/cart/cart.selectors'
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.styles.scss'
@@ -16,7 +17,7 @@ const CartDropdown = ({cartItems}) => (
 )
 
 const mapStateToProps = state => ({
-  cartItems: state.cart.cartItems
+  cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown)
